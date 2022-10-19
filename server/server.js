5bd362e7eedef9ee0f8481309e510d5f49fc75a7
“abdul-san” <kamara847@gmail.com>
2022-10-19 17:55:18 +0100

    added css styling

--- server/server.js
+++ server/server.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json())
 // app.use(cors());
 
-const port = 5007;
+// app.set('view engine', 'ejs');
+// let ejs = require('ejs');   
+
+const port = 5008;
 app.listen(port, () => console.log(`Listening on part ${port}`))
 
 let cars = [
@@ -14,15 +17,29 @@ let cars = [
     {id : 3, "car": "Volkswagen"},
     {id : 4, "car": "Bentley"}]
 
+function getRandomCar(){
+    let i = Math.floor((Math.random() * cars.length+1));
+    return cars[i]
+    
+}
+
+// app.get("/cars/random", function(req, res) {
+//     res.type("text/plain")
+//     let rn = Math.floor(Math.random() * cars.length+1);
+//     res.send(cars[rn])  
+// })
+
 app.get("/cars", (req, res) => {
     res.status(200).send(cars)
+    res.render('cars.html')
 })
+app.get("/cars/random", (req, res) => res.send(getRandomCar()))
 
-app.get("/cars/random", function(req, res) {
-    res.type("text/plain")
-    let rn = Math.floor(Math.random() * cars.length+1);
-    res.send(cars[rn])
 
-    
-})
+// app.get("/cars/random", function(req, res) {
+//     res.type("text/plain")
+//     let rn = Math.floor(Math.random() * cars.length+1);
+//     res.send(cars[rn])
+// })
 // for every car in cars (.length), return a random index
+// app.listen(port, ()=>console.log("the server is running "))
